29f810ee508af20386cc6e952be48135938db6ba
eveee00 <83956322+eveee00@users.noreply.github.com>
2024-10-20 06:06:53 +0200

    Save the image and data to a folder

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import requests
-r = requests.get('https://computernewb.com/vncresolver/api/scans/vnc/random')
-print(r.text)
+import json
+import os
+
+# gets the random vnc id
+r = requests.get('https://computernewb.com/vncresolver-next/api/v1/random')
+data = r.json()
+id_value = data.get('id')
+print(id_value)
+
+# gets the image
+img = requests.get(f'https://computernewb.com/vncresolver-next/api/v1/screenshot/{id_value}')
+os.makedirs('result', exist_ok=True)
+os.makedirs(f'result/{id_value}', exist_ok=True)
+
+# saves the image
+with open(f'result/{id_value}/vnc_{id_value}.png', 'wb') as f:
+    f.write(img.content)
+
+# saves the data to a json file with the same name as the image
+with open(f'result/{id_value}/vnc_{id_value}.json', 'w') as f:
+    json.dump(data, f, indent=4)
